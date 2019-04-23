60cf6469a5db7106195508b97c47b98cd9506d17
Leo Dasso <leo.dasso@gmail.com>
2019-04-23 16:26:07 -0500

    add string query to get event id in followup form

--- src/components/FollowUp/FollowUp.js
+++ src/components/FollowUp/FollowUp.js
@@ -1,25 +1,42 @@
 import React, { Component } from "react";
 import { connect } from 'react-redux';
 import axios from 'axios';
+import queryString from 'query-string';
 
 class FollowUp extends Component {
 
     state = {
-        eventId: 2,
         event: {},
+        queryOk: false,
     }
 
     componentDidMount() {
+
+        // The event ID is provided in the URL as a query. We find which event to display using this.
+        let url = window.location.href;
+        url = url.replace('#', '');
+        const queries = queryString.parseUrl(url).query;
+
+        // If there was no event query in the URL, then remember that 
+        // the query isnt ok. We'll just render something to direct the user to home page.
+        if (!queries.event) {
+            console.log('No query found for event id.')
+            return;
+        }
+
+        this.setState({queryOk: true});
+        const eventId = queries.event;
 
         // get the event info for the selected event
         axios({
             method: 'GET',
             url: '/api/events/specific',
             params: {
-                id: this.state.eventId,
+                id: eventId,
             }
         })
         .then( response => {
+            console.log('found event', response.data[0]);
             this.setState({event: response.data[0]})
         })
         .catch( error => {
