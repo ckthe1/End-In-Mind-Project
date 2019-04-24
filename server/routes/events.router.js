54f59051c221f22ae0438d99f43f187f7fc0c1b6
Leo Dasso <leo.dasso@gmail.com>
2019-04-23 19:14:49 -0500

    event followup works now but its ugly

--- server/routes/events.router.js
+++ server/routes/events.router.js
@@ -28,11 +28,20 @@ router.get('/specific', rejectUnauthenticated, (req, res) => {
 
   console.log("in get specific event route", req.query);
 
-  pool.query(`SELECT * FROM "events" 
-  JOIN "communities" ON "events"."community_id" = "communities"."id"
+  pool.query(`
+  SELECT "events"."id", "event_name", "event_type", "location", 
+"events"."description", "contact_name", "contact_phone", "events"."created_at", 
+"author_user_id", "follow_up_complete",
+"follow_up_comments", "start_time", "end_time", 
+"communities"."id" as "community_id", 
+"communities"."name" as "community_name"
+ FROM "events" 
+  JOIN "communities" ON "events"."community_id" = "communities"."id" 
   WHERE "events"."id" = $1;`, [req.query.id])
 
   .then (response => {
+
+    console.log(response);
 
     // for calendar to read the dates, they need to be converted from strings to Date objects
     const convertedEvents = response.rows.map( event => convertEvent(event))
@@ -56,6 +65,27 @@ function convertEvent(rawEvent) {
   newEvent.community = rawEvent.name;
   return newEvent;
 }
+
+router.put('/followup', (req, res) => {
+
+  console.log('in followup route', req.query);
+  
+  pool.query(`
+    UPDATE "events"
+    SET "follow_up_comments" = $1
+    WHERE "id" = $2;`, 
+    [req.query.comments, req.query.eventId])
+
+  .then(result => {
+    console.log("everything is just dandy");
+    res.sendStatus(200);
+  })
+
+  .catch(error => {
+    console.log("error in followup route", error);
+    res.sendStatus(500);
+  });
+})
 
 
 // Patrick's get request that grabs all users contact info from the selected community 
--- src/components/FollowUp/FollowUp.js
+++ src/components/FollowUp/FollowUp.js
@@ -8,9 +8,14 @@ class FollowUp extends Component {
     state = {
         event: {},
         queryOk: false,
+        comments: '',
     }
 
     componentDidMount() {
+        this.refreshEvent();
+    }
+
+    refreshEvent = () => {
 
         // The event ID is provided in the URL as a query. We find which event to display using this.
         let url = window.location.href;
@@ -36,29 +41,89 @@ class FollowUp extends Component {
             }
         })
         .then( response => {
-            console.log('found event', response.data[0]);
-            this.setState({event: response.data[0]})
+
+            // If there were no events with the given ID, update the state
+            // so we can let the user now.
+            if (response.data.length < 1) {
+                this.setState({queryOk: false});
+
+            // If there were events found, (it really should only be 1 event)
+            // then put the event in state so we can display the feedback form.
+            }else {
+                this.setState({event: response.data[0]})
+            }
         })
         .catch( error => {
             console.log(error);
         })
     }
 
+    onCommentsChanged = event => {
+        this.setState({comments: event.target.value});
+    }
+
+    // The parameter 'event' here refers to the DOM event, not like, one of our calendar events
     handleSubmit = (event) => {
         event.preventDefault();
-        console.log("handling submit");
-        // TODO actually handle the event
+
+        if (this.state.comments.length < 1) {
+            alert('Please enter your comments before submitting!');
+            return;
+        }
+
+        console.log(this.state.event);
+
+        axios({
+            method: 'PUT',
+            url: '/api/events/followup',
+            params: {
+                eventId: this.state.event.id,
+                comments: this.state.comments,
+            }
+        })
+        .then(response => {
+            this.setState({comments: ''});
+            this.refreshEvent();
+        })
+
+        .catch(error => {
+            console.log('error submitting followup', error);
+            alert("Something went wrong! Please try again in a few minutes");
+        })
     }
 
     render() {
 
+        // Render for missing events
+        if (!this.state.queryOk) {
+            return (
+                <div>
+                    <h1>Event Not Found</h1>
+                    <p>It looks like the event you're looking for isn't here!</p>
+                </div>
+            )
+        }
+
+        const event = this.state.event;
+
+        // If the event already has a followup - we determine this by checking if
+        // followup comments is null
+        if (event.follow_up_comments != null) {
+            return (
+                <div>
+                    <h2>Thanks for your feedback!</h2>
+                </div>
+            )
+        }
+
         return (
             <div>
-                <h1>Followup for {this.state.event.event_name}</h1>
+                <h1>Followup for {event.event_name}</h1>
+                <p>{event.description}</p>
                 <form onSubmit={this.handleSubmit}>
-                    <input  placeholder='event type'/>
-                    <input  type="Number" placeholder='estimated attendees'/>
-                    <input placeholder='comments' /> 
+                    <input placeholder='comments' 
+                        value={this.state.comments}
+                        onChange={this.onCommentsChanged}/> 
                     <button>Submit</button>
 
                 </form>
